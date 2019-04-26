3cb0991b6e4632b583147dbb13d73e67e52c9298
Jordan Gold <jordandgold@gmail.com>
2019-04-25 21:56:01 -0600

    update component structure

--- src/App.js
+++ src/App.js
@@ -1,51 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // components
-import RecipeList from './RecipeList/RecipeList';
-import RecipeDetail from './RecipeDetail/RecipeDetail';
-import AddRecipe from './AddRecipe/AddRecipe';
+import RecipeList from "./RecipeList/RecipeList";
+import RecipeDetail from "./RecipeDetail/RecipeDetail";
+import AddRecipe from "./AddRecipe/AddRecipe";
 
 // bootstrap
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import AddRecipeForm from "./AddRecipeForm/AddRecipeForm";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-	constructor(props) {
-		super(props)
+    this.state = {
+      isAddingRecipe: false,
+      selectedRecipe: false,
+      recipes: [
+        {
+          name: "Black Bean Enchiladas"
+        },
+        {
+          name: "Baked Falafel Wraps"
+        }
+      ]
+    };
+  }
 
-		this.state = {
-			isAddingRecipe: false,
-			selectedRecipe: false,
-			recipes: [
-				{
-					name: 'Black Bean Enchiladas',
-				},
-				{
-					name: 'Baked Falafel Wraps',
-				}
-			]
-		}
-	}
+  handleAddRecipe = () => {
+    this.setState({ isAddingRecipe: true });
+  };
 
-	render() {
-	    return (
-	      <div className="App">
-				<Container fluid="true">
-					<Row>
-						<Col xs="12" md="3">
-							<AddRecipe isAddingRecipe={this.state.isAddingRecipe} />
-							<RecipeList recipes={this.state.recipes} />
-						</Col>
-						<Col xs="12" md="auto">
-							<RecipeDetail recipe={this.state.selectedRecipe} />
-						</Col>
-					</Row>
-				</Container>
-	      </div>
-	    );
-	}
+  render() {
+    return (
+      <div className="App">
+        <Container fluid="true">
+          <Row>
+            <Col xs="12" md="3">
+              <Button variant="primary" onClick={this.handleAddRecipe}>
+                Add Recipe
+              </Button>
+              <RecipeList recipes={this.state.recipes} />
+            </Col>
+            <Col xs="12" md="auto">
+              {this.state.isAddingRecipe ? <AddRecipeForm /> : ""}
+              <RecipeDetail recipe={this.state.selectedRecipe} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/RecipeList/RecipeList.js
+++ src/RecipeList/RecipeList.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import RecipeListItem from '../RecipeListItem/RecipeListItem';
+import React, { Component } from "react";
 
 class RecipeList extends Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		return (
-			<ul className="recipe-list list-group">
-				<h4>My Recipes</h4>
-				{this.props.recipes.map((recipe) => {
-					return <RecipeListItem recipe={recipe} />
-				})}
-			</ul>
-		);
-	}
+  render() {
+    return (
+      <ul className="recipe-list list-group">
+        <h4>My Recipes</h4>
+        {this.props.recipes.map(recipe => {
+          return <li className="recipe-list list-group-item">{recipe.name}</li>;
+        })}
+      </ul>
+    );
+  }
 }
 
 export default RecipeList;
